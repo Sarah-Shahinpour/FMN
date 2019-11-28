7e04294e51978619b91c6458c32ba91640dd0b7a
Sarah Shahinpour <sarahshahinpour@Sarahs-MacBook-Air.local>
2019-11-27 21:19:12 -0700

    first commit

---
+++ FMN/AddJournalEntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddJournalEntryViewController.swift
+//  FMN
+//
+//  Created by Sarah Shahinpour on 11/27/19.
+//  Copyright © 2019 Sarah Shahinpour. All rights reserved.
+//
+
+import UIKit
+
+class AddJournalEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FMN/JournalEntry.swift
@@ -0,0 +1,13 @@
+//
+//  JournalEntry.swift
+//  FMN
+//
+//  Created by Sarah Shahinpour on 11/25/19.
+//  Copyright © 2019 Sarah Shahinpour. All rights reserved.
+//
+
+import UIKit
+
+class JournalEntry: NSObject {
+
+}
